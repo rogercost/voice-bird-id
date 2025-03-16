b9e10d041de8cb635f611d1f37a8ecf2f4eda0f7
Roger Cost <cost.roger@gmail.com>
2025-03-16 17:39:13 -0400

    quality control: remove preliminary headings if they appear

--- data_prep/collect_synth_rec_embeddings.py
+++ data_prep/collect_synth_rec_embeddings.py
@@ -55,8 +55,12 @@ for family, bird_list in aba_checklist.items():
 
         used_recordings = []
         for recording in recordings:
-            if len(recording) > 0 and not recording.isspace():
+            if len(recording) > 100 and not recording.isspace():
                 used_recordings.append(recording)
+
+        # Sometimes we get preliminary content at the beginning. We prompt the LLM not to include it but still sometimes
+        # get it. So since we know we asked for 20, take the last 20, excluding anything preliminary.
+        used_recordings = used_recordings[-20:]
 
         try:
             time.sleep(5)
